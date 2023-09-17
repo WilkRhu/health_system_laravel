d19f057edd2e14ad56df5953ec1874ba34b32a27
Wilk <wilk.caetano@gmail.com>
2023-09-16 22:59:52 -0300

    fix: corrections testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -7,14 +7,4 @@ use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
-    public function createApplication()
-    {
-        $app = require __DIR__.'/../bootstrap/app.php';
-
-
-        $app['config']->set('database.mysql_test', 'testing');
-
-        return $app;
-    }
 }
